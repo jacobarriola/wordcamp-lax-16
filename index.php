afc55ff17d7aed7a7b57b169cf03c2cbd51ab1ce
Jacob Arriola <jacob@Jacobs-MacBook-Pro.local>
2016-08-08 20:33:54 -0700

    set up testimonial component

--- index.php
+++ index.php
@@ -13,6 +13,32 @@
 
 get_header(); ?>
 
-hellos
+<div class="row column">
+	<div>Header component</div>
+
+
+	<div>
+		<h2>Testimonial component</h2>
+		<div class="testimonial-container">
+			<div class="testimonial-img">
+				<img src="https://fillmurray.com/150/150" alt="">
+			</div>
+			<div class="testimonial-name">
+				<h3>Steve Zehngut</h3>
+				<div>Zeek Interactive</div>
+				<p>Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+
+	Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+			</div>
+		</div>
+	</div>
+
+
+	<div>Logos</div>
+	<div>Footer component</div>
+
+</div>
+
+
 
 <?php get_footer(); ?>
